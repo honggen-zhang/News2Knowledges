29fede3cf1492214320bd785c1e6efcb024d2a5e
honggen-zhang <56853824+honggen-zhang@users.noreply.github.com>
2021-09-17 11:57:02 -1000

    Update node_cler_frequence.py
    add a part about top nodes

--- node_cler_frequence.py
+++ node_cler_frequence.py
@@ -27,7 +27,8 @@ def write_file(str,all_triplets):
         for node in all_triplets:
             a = node.split('@')
             #print(node)
-            writer.writerow({'head': a[0], 'relation': a[1],'tail':a[2]})
+            if a[0] in node_most_common_500 and a[2] in node_most_common_500 and a[1] in edge_most_common_500:
+                writer.writerow({'head': a[0], 'relation': a[1],'tail':a[2]})
 
 def read_file(str):
     nodes = []
@@ -43,17 +44,22 @@ def read_file(str):
         #if len(a)>0:
         id2node[a[0]] = label
         node2id[label] = a[0]
+        #if label == 'jussie' or label ==
         nodes.append(label)
+    print('read done')
     return nodes,id2node,node2id         
 
 def IntersectionWord(node_list):
     dup_dic = {}
-    
+    print('begainning diction...')
     #node_list2 = node_list
-    while len(node_list)>=1:
+    print(len(node_list))
+    while len(node_list)>=2:
+        #print(node_list)
         node1  = node_list[0]
         node1_dup = []
         node_list.remove(node1)
+        #print(node1)
         node_list2 = node_list.copy()        
         for node2 in node_list2:
             a = node1.split()
@@ -66,17 +72,48 @@ def IntersectionWord(node_list):
                 ratio1 = len(inter)/len(a)
                 ratio2 = len(inter)/len(b)
                 ratio  = min(ratio1,ratio2)
-                if ratio>=0.5:
+                if ratio>0.5:
                     node1_dup.append(node2)
                     node_list.remove(node2)
         dup_dic[node1] = node1_dup
+    print('diction done')
+    return dup_dic
+
+def IntersectionWord2(node_list):
+    dup_dic = {}
+    print('begainning diction...')
+    #node_list2 = node_list
+    print(len(node_list))
+    while len(node_list)>=2:
+        node1  = node_list[0]
+        node1_dup = []
+        node_list.remove(node1)
+        print(node1)
+        node_list2 = node_list.copy()        
+        for node2 in node_list2:
+            
+            a = node1.split()
+            b = node2.split()
+            inter = list(set(a).intersection(set(b)))
+
+            if set(inter).issubset(set(stop_words)):
+                pass
+            else:
+                ratio1 = len(inter)/len(a)
+                ratio2 = len(inter)/len(b)
+                ratio  = min(ratio1,ratio2)
+                if ratio>0.5:
+                    node1_dup.append(node2)
+                    node_list.remove(node2)
+        dup_dic[node1] = node1_dup
+    print('diction done')
     return dup_dic
 
 def IntersectionEdge(node_list):
     dup_dic = {}
     
     #node_list2 = node_list
-    no_list = ["not","nt","don't"]
+    no_list = ["not","nt","dont"]
     while len(node_list)>=1:
         node1  = node_list[0]
         node1_dup = []
@@ -87,13 +124,14 @@ def IntersectionEdge(node_list):
             b = node2.split()
             inter = list(set(a).intersection(set(b)))
 
-            if set(inter).issubset(set(stop_words)) or len(set(no_list).intersection(set(a))) != 0:
+            if set(inter).issubset(set(stop_words)) or 'not' in a:
                 pass
             else:
                 ratio1 = len(inter)/len(a)
                 ratio2 = len(inter)/len(b)
                 ratio  = min(ratio1,ratio2)#for edge
-                if ratio>=0.5:
+                if ratio>0.5:
+                    #print(node1,node2)
                     node1_dup.append(node2)
                     node_list.remove(node2)
         dup_dic[node1] = node1_dup
@@ -101,7 +139,7 @@ def IntersectionEdge(node_list):
 
 #The section of cluster similarity words together based on word itself
 
-node_list,id2node_dic,node2id_dic = read_file('/home/user1/deskdata/JS/data2/node_code_core_cle.csv') # the all nodes load from coreference node code
+node_list,id2node_dic,node2id_dic = read_file('/home/user1/Desktop/experments_reuslts/data_generator/JS/node_code_core_cle.csv') # the all nodes load from coreference node code
 duplication_dic = IntersectionWord(node_list) # cluster node
 new_id2node = id2node_dic.copy()
 for ker, value in duplication_dic.items():# build new diction to storage id and label
@@ -110,7 +148,7 @@ for ker, value in duplication_dic.items():# build new diction to storage id and 
         idx = node2id_dic [node] 
         new_id2node[idx] = ker
 #-------------------------------------------------------------------------------
-edge_list,id2edge_dic,edge2id_dic = read_file('/home/user1/deskdata/JS/data2/edge_code_core_cle.csv')     
+edge_list,id2edge_dic,edge2id_dic = read_file('/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_code_core_cle.csv')     
 duplication_dic_edge = IntersectionEdge(edge_list) #cluster edge
 new_id2edge = id2edge_dic.copy()
 for ker, value in duplication_dic_edge.items():
@@ -120,21 +158,23 @@ for ker, value in duplication_dic_edge.items():
         new_id2edge[idx] = ker
 #-----------------------------------------------------------------------------------------
 #The part trys to rewrite the triples based on above node and edge cluster
-filelist = os.listdir(r'/home/user1/deskdata/JS/data2/coreference_clearnoise/')
+filelist = os.listdir(r'/home/user1/Desktop/experments_reuslts/data_generator/JS/coreference_clearnoise/')
 filelist.sort()
 
 time = []
-#node_list = []
-#edge_list = []
+node_list = []
+edge_list = []
 #all_node = []
+daily_triples_set = []
 for j in range(len(filelist)):
     tmpfn=str(filelist[j])
-    ##print(tmpfn)
+    tmpfn = 'file_'+str(j)+'.csv'
+    print(tmpfn)
     time.append(tmpfn[:-4])
     triplet_set = []
     
 
-    content_test = open('/home/user1/deskdata/JS/data2/coreference_clearnoise/'+tmpfn, "r").readlines()
+    content_test = open('/home/user1/Desktop/experments_reuslts/data_generator/JS/coreference_clearnoise/'+tmpfn, "r").readlines()
     for i in range(1,len(content_test)):
         lines = content_test[i]
         a=lines.split(',')
@@ -144,12 +184,36 @@ for j in range(len(filelist)):
         idx_head = node2id_dic[head]
         idx_tail = node2id_dic[tail]
         idx_relation = edge2id_dic[relation]
+        
         new_head = new_id2node[idx_head]
+        if new_head == 'jussie' or new_head =='smollett':
+            new_head = 'jussie smollett'
+        node_list.append(new_head)
+        
         new_tail = new_id2node[idx_tail]
+        if new_tail == 'jussie' or new_tail =='smollett':
+            new_tail = 'jussie smollett'          
+        node_list.append(new_tail)
+      
         new_relation = new_id2edge[idx_relation]
+        edge_list.append(new_relation)
+        
         if new_head != new_tail:
             triple = new_head+'@'+new_relation+'@'+new_tail
         triplet_set.append(triple)
-    write_file('/home/user1/deskdata/JS/data2/edge_fre_clear/'+tmpfn,list(set(triplet_set)))
+    daily_triples_set.append(triplet_set)
+node_most_common_500_dic = Counter(node_list).most_common(5000)
+edge_most_common_500_dic = Counter(edge_list).most_common()
+
+node_most_common_500 = []
+edge_most_common_500 = []
+for node in node_most_common_500_dic:
+    node_most_common_500.append(node[0])
+for edge in edge_most_common_500_dic:
+    edge_most_common_500.append(edge[0])
+for k in range(len(filelist)):
+    triplet_set = daily_triples_set[k]
+    tmpfn = 'file_'+str(k)+'.csv'
+    write_file('/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_fre_clear2/'+tmpfn,list(set(triplet_set)))
 
 
