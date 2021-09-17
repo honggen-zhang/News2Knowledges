ea7d5bb6e0ecf64c862f0f7ff3aa09971b418089
honggen-zhang <56853824+honggen-zhang@users.noreply.github.com>
2021-09-17 11:58:09 -1000

    Update node_cler_embedding.py

--- node_cler_embedding.py
+++ node_cler_embedding.py
@@ -5,9 +5,9 @@ Created on Thu Apr 15 23:20:22 2021
 
 @author: user1
 """
-
 import gensim 
 from gensim.models import Word2Vec 
+from nltk.stem.wordnet import WordNetLemmatizer
 import os
 from collections import Counter
 import matplotlib.pyplot as plt
@@ -18,6 +18,7 @@ from nltk.corpus import stopwords
 stop_words = list(stopwords.words('english'))
 import gensim 
 from gensim.models import Word2Vec 
+import numpy as np
 
 def write_KG(str,all_triplets):
     with open(str, 'w', newline='') as csvfile:
@@ -93,41 +94,112 @@ def encode_node(str):
             edge_list.append(relation)              
             tail = a[2][:-1]
             node_list.append(tail)
-            if head != tail:
-                triple = head+'@'+relation+'@'+tail
-                if triple not in triplet_list:
-                    triplet_list.append(triple)
+            #if head != tail:
+            triple = head+'@'+relation+'@'+tail
+            if triple not in triplet_list:
+                triplet_list.append(triple)
     print(len(triplet_list))
     topnodes = Counter(node_list).most_common()
     topedges = Counter(edge_list).most_common()
     #print(topnodes)
-    write_code('/home/user1/deskdata/JS/data2/node_code_frequence_cle.csv',list(set(node_list)))
-    write_code('/home/user1/deskdata/JS/data2/edge_code_frequence_cle.csv',list(set(edge_list)))
+    write_code('/home/user1/Desktop/experments_reuslts/data_generator/JS/node_code_frequence_cle.csv',list(set(node_list)))
+    write_code('/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_code_frequence_cle.csv',list(set(edge_list)))
     return topnodes,topedges
 def sim(phrase1, phrase2):
     p1 = phrase1.split()
     p2 = phrase2.split()
     score = 0
-    n = 0.01
-    for w1 in p1:
-        if w1 not in stop_words:
+    n = 0.001
+    if len(set(p1).intersection(set(p2))) != 0:
+        #print(p1)
+        #print(p2)
+        for w1 in p1:
+        #if w1 not in stop_words:
             n = n+1
-            score_list = [0.0]
+            score_list = []
             for w2 in p2:
-                if w2 not in stop_words:
-                    try:
-                        s_tmp = model.wv.similarity(w1,w2)
-                        score_list.append(s_tmp)
-                    except:
-                        score_list.append(0.0)
-            w1_s_m = max(score_list)
+            #if w2 not in stop_words:
+                try:
+                    s_tmp = model.wv.similarity(w1,w2)
+                    score_list.append(s_tmp)
+                except:
+                    score_list.append(0.0)
+            w1_s_m = np.average(score_list)
+            #print(w1,p2,score_list)
             score = score + w1_s_m
         
     return score/n
                 
-                
-        
+def sim_edge(phrase1, phrase2):
+    p1 = phrase1.split()
+    p2 = phrase2.split()
+    score = 0
+    n = 0.001
     
+        #print(p1)
+        #print(p2)
+    for w1 in p1:
+        score_list = []
+        if w1 not in stop_words:
+# =============================================================================
+#             try:
+#                 w1 = WordNetLemmatizer().lemmatize(w1,'v')
+#             except:
+#                 w1 = w1
+# =============================================================================
+            n = n+1
+                #score_list = []
+            for w2 in p2:
+                if w2 not in stop_words:
+# =============================================================================
+#                     try:
+#                         w2 = WordNetLemmatizer().lemmatize(w2,'v')
+#                     except:
+#                         w2 = w2
+# =============================================================================
+                    
+                    if w1 == w2:
+                        s_tmp = 1
+                    else:
+                        try:
+                            s_tmp = model.wv.similarity(w1,w2)
+                            
+                        except:
+                            s_tmp = 0.0
+                    score_list.append(s_tmp)
+                w1_s_m = np.average(score_list)
+                #print(w1,p2,score_list)
+                score = score + w1_s_m
+        
+    return score/n                
+        
+def sim_dis1(keynodes,allnodes):
+    dic_sim = {}
+    while len(keynodes)>=1:
+        node1 = keynodes[0]
+        keynodes.remove(node1)
+        try:
+            allnodes.remove(node1)
+        except:
+            pass
+        nodes2 = allnodes.copy()
+        tmp_list = []
+        #print('node1:--',node1)
+        for node2 in nodes2:
+            sim_bw = sim(node1,node2)
+            if sim_bw>=0.45:
+                #print(node2)
+                if len(tmp_list)<50:
+                    allnodes.remove(node2)
+                    tmp_list.append(node2)
+                    try:
+                        keynodes.remove(node2)
+                    except:
+                        pass
+        dic_sim[node1] = tmp_list
+    for node in allnodes:
+        dic_sim[node]=[]
+    return dic_sim    
 
 def sim_dis(nodes):
     dic_sim = {}
@@ -139,9 +211,9 @@ def sim_dis(nodes):
         
         for node2 in nodes2:
             sim_bw = sim(node1,node2)
-            if sim_bw>=0.8:
+            if sim_bw>=0.6:
                 #print(node2)
-                if len(tmp_list)<10:
+                if len(tmp_list)<50:
                     nodes.remove(node2)
                     tmp_list.append(node2)
         dic_sim[node1] = tmp_list
@@ -155,28 +227,35 @@ def sim_dis_edge(nodes):
         tmp_list = []
         
         for node2 in nodes2:
-            sim_bw = sim(node1,node2)
-            if sim_bw>=0.8:
+            sim_bw = sim_edge(node1,node2)
+            #print(node1,'--',node2,'--',sim_bw)
+            if sim_bw>=0.5:
                 #print(node2)
-                if len(tmp_list)<10:
+                if len(tmp_list)<50:
                     nodes.remove(node2)
                     tmp_list.append(node2)
         dic_sim[node1] = tmp_list
     return dic_sim
 
-model = gensim.models.Word2Vec.load('/home/user1/deskdata/JS/data2/model_JS2')
-topnode_frequence, topedge_frequence= encode_node('/home/user1/deskdata/JS/data2/edge_fre_clear/')
+model = gensim.models.Word2Vec.load('/home/user1/Desktop/experments_reuslts/data_generator/JS/model_JS1')
+topnode_frequence, topedge_frequence= encode_node('/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_fre_clear2/')
 #=====================================================================================
 
 #node_list,id2node_dic,node2id_dic = read_file('/home/user1/deskdata/JS/data1/node_code_frequence_cle.csv')
 new_node_list = []
+keynodes_list = []
 for tf in topnode_frequence:
-    if tf[1]>2:
-        new_node_list.append(tf[0])
+    #if tf[1]>2:
+    new_node_list.append(tf[0])
+    if tf[1]>10:
+        keynodes_list.append(tf[0])
+        
 print(len(new_node_list))
-new_node_list1 = new_node_list.copy()
-cluster = sim_dis(new_node_list1)
 
+new_node_list1 = new_node_list.copy()
+keynodes = keynodes_list.copy()
+#cluster = sim_dis(new_node_list1)
+cluster = sim_dis1(keynodes,new_node_list1)
 old2new_dic = {}
 for key, value in cluster.items():
     old2new_dic[key] = key
@@ -185,11 +264,13 @@ for key, value in cluster.items():
             old2new_dic[node] = key
     except:
         pass
+    
+
 #--------------------------------------------------------------------------------------------------------
 new_edge_list = []
 for tf in topedge_frequence:
-    if tf[1]>1:
-        new_edge_list.append(tf[0])
+    #if tf[1]>1:
+    new_edge_list.append(tf[0])
 print(len(new_edge_list))
 new_edge_list1 = new_edge_list.copy()
 cluster_edge = sim_dis_edge(new_edge_list1)
@@ -202,8 +283,9 @@ for key, value in cluster_edge.items():
             old2new_edge_dic[node] = key
     except:
         pass
+
 #----------------------------------------------------------------------------------------------
-filelist = os.listdir(r'/home/user1/deskdata/JS/data2/edge_fre_clear/')
+filelist = os.listdir(r'/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_fre_clear2/')
 filelist.sort()
 
 #time = []
@@ -211,14 +293,14 @@ node_list = []
 edge_list = []
 all_node = []
 for j in range(len(filelist)):
-    #tmpfn=str(filelist[j])
+    tmpfn=str(filelist[j])
     tmpfn = 'file_'+str(j)+'.csv'
-    ##print(tmpfn)
     #time.append(tmpfn[:-4])
+    print(tmpfn)
     triplet_set = []
     
 
-    content_test = open('/home/user1/deskdata/JS/data2/edge_fre_clear/'+tmpfn, "r").readlines()
+    content_test = open('/home/user1/Desktop/experments_reuslts/data_generator/JS/edge_fre_clear2/'+tmpfn, "r").readlines()
     for i in range(1,len(content_test)):
         lines = content_test[i]
         a=lines.split(',')
@@ -236,6 +318,6 @@ for j in range(len(filelist)):
         except:
             triplet_set = triplet_set
     #write_file('/home/user1/deskdata/JS/data2/edge_after_embedding/'+tmpfn,list(set(triplet_set)))
-    write_KG('/home/user1/deskdata/JS/data2/knowledge_graph/file_'+str(j+1)+'.csv',list(set(triplet_set)))
+    write_KG('/home/user1/Desktop/experments_reuslts/data_generator/JS/knowledge_graph2/file_'+str(j+1)+'.csv',list(set(triplet_set)))
     
     
